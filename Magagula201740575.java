95299c619cb43647c188257e63dbe59ac1b9a58a
siyabonga <princesiyabonga9@gmail.com>
2024-05-07 06:17:59 -0700

    first commit

---
+++ Magagula201740575.java
@@ -0,0 +1,13 @@
+public class Doe12345 {
+    public static void main(String[] args) {
+        // Define two numbers to add
+        int number1 = null;
+        int number2 = null;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Print the sum
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
